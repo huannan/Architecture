3c9383b520197aaac514e27ebbc3eed56ce4c783
吴焕楠 <wuhuannan@meizu.com>
2020-10-22 08:41:49 +0800

    属性动画

--- day13_pattern_strategy/src/main/java/com/nan/day13_pattern_strategy/MainActivity.java
+++ day13_pattern_strategy/src/main/java/com/nan/day13_pattern_strategy/MainActivity.java
@@ -1,7 +1,9 @@
 package com.nan.day13_pattern_strategy;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 import timber.log.Timber;
 
@@ -11,6 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 自己实现一个可以这样做：通过线程不断地去改变属性
+        ImageView imageView = null;
+        ObjectAnimator animator = ObjectAnimator.ofFloat(imageView, "", 1.0F);
+        animator.setDuration(400L);
+        animator.start();
+
         // 默认使用当前类名为TAG
         // Timber.tag("MainActivity");
         Timber.d("Hello %s!", "huannan");
