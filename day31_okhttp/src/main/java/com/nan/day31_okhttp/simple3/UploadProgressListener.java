f618153072f57b5c9682827cf1e91e4d65c9d7fa
吴焕楠 <wuhuannan@meizu.com>
2020-12-10 17:40:54 +0800

    RxJava

--- day31_okhttp/src/main/java/com/nan/day31_okhttp/simple3/UploadProgressListener.java
+++ day31_okhttp/src/main/java/com/nan/day31_okhttp/simple3/UploadProgressListener.java
@@ -1,8 +1,7 @@
 package com.nan.day31_okhttp.simple3;
 
-/**
- * Created by hcDarren on 2017/11/25.
- */
 public interface UploadProgressListener {
+
     void onProgress(long total, long current, int progress);
+
 }
